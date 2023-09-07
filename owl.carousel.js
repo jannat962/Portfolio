39b780c9004c7a4549016edfd5514ce723a474ab
Jannatul__khanam <102294457+jannat962@users.noreply.github.com>
2023-09-07 14:41:35 +0600

    Update owl.carousel.js

--- owl.carousel.js
+++ owl.carousel.js
@@ -1,16 +1,3 @@
-/*
- *  jQuery OwlCarousel v1.3.2
- *
- *  Copyright (c) 2013 Bartosz Wojciechowski
- *  http://www.owlgraphic.com/owlcarousel/
- *
- *  Licensed under MIT
- *
- */
-
-/*JS Lint helpers: */
-/*global dragMove: false, dragEnd: false, $, jQuery, alert, window, document */
-/*jslint nomen: true, continue:true */
 
 if (typeof Object.create !== "function") {
     Object.create = function (obj) {
