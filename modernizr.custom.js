1df5b46dfc976911253913b0777bfe81e8e1bc18
Jannatul__khanam <102294457+jannat962@users.noreply.github.com>
2023-09-07 14:39:07 +0600

    Update modernizr.custom.js

--- modernizr.custom.js
+++ modernizr.custom.js
@@ -1,6 +1,4 @@
-/* Modernizr 2.8.3 (Custom Build) | MIT & BSD
- * Build: http://modernizr.com/download/#-fontface-backgroundsize-borderimage-borderradius-boxshadow-flexbox-flexboxlegacy-hsla-multiplebgs-opacity-rgba-textshadow-cssanimations-csscolumns-generatedcontent-cssgradients-cssreflections-csstransforms-csstransforms3d-csstransitions-applicationcache-canvas-canvastext-draganddrop-hashchange-history-audio-video-indexeddb-input-inputtypes-localstorage-postmessage-sessionstorage-websockets-websqldatabase-webworkers-geolocation-inlinesvg-smil-svg-svgclippaths-touch-webgl-shiv-cssclasses-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-load
- */
+
 ;window.Modernizr = function(a, b, c) {
     function C(a) {
         j.cssText = a
